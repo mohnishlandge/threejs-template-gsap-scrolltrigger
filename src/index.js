2c2fe74b54926ce16d30179a16126ca591fa62bc
Mohnish Landge <tutume1999@gmail.com>
2021-04-30 18:39:40 +0530

    up working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style/main.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 /**
  * GUI Controls
  */
@@ -86,6 +88,47 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animation GSAP
+ */
+
+gsap.registerPlugin(ScrollTrigger)
+ScrollTrigger.defaults({
+  scrub: 3,
+  ease: 'none',
+})
+const sections = document.querySelectorAll('.section')
+gsap.from(mesh.position, {
+  y: 1,
+  duration: 1,
+  ease: 'expo',
+})
+gsap.from('h1', {
+  yPercent: 100,
+  autoAlpha: 0,
+  ease: 'back',
+  delay: 0.3,
+})
+gsap.to(mesh.rotation, {
+  x: Math.PI * 2,
+  scrollTrigger: {
+    trigger: sections[1],
+  },
+})
+gsap.to(mesh.scale, {
+  x: 0.3,
+  y: 0.3,
+  scrollTrigger: {
+    trigger: sections[2],
+  },
+})
+gsap.to(mesh.rotation, {
+  y: Math.PI * 2,
+  scrollTrigger: {
+    trigger: sections[3],
+  },
+})
 
 /**
  * Animate
